292ae416393f836e66ee0686cf5a70eb8faab189
ChenChuanan <https://github.com/chuanan/wenda.git>
2017-12-25 22:08:03 +0800

    初始化项目

---
+++ src/main/iamxb.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by ChenChuanan on 2017/12/25.
+ */
+public class iamxb {
+    public static void main(String[] args){
+        System.out.println("Hello World!dsf");
+    }
+}
